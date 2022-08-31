f99dcf3046677065f6ad730bfd12d9ce28111789
Keisha Hepner <kmhepner90@gmail.com>
2022-08-31 16:10:06 -0700

    Pass first three tests
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>
    
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>
    Co-authored-by: William Mentzer <william.mentzer2k@gmail.com>

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -1,11 +1,24 @@
 import { Haiku } from './../src/scripts.js';
 
 describe('Haiku', () => {
+ let haiku;
+ let haikuArray;
+  beforeEach(() => {
+   haiku = new Haiku("An old pond!\n A frog jumps in\n the sound of water.");
+   haikuArray = haiku.assignLineKeys();
+  });
   
   test('should create a Haiku object with user input', () => {
-    const haiku = new Haiku("An old pond! A frog jumps in the sound of water.")
-    expect(haiku.haikuInput).toEqual("An old pond! A frog jumps in the sound of water.");
+    expect(haiku.haikuInput).toEqual("An old pond!\n A frog jumps in\n the sound of water.");
   });
+
+  test('should create an array from the haiku', () => {
+    expect(haikuArray[0]).toEqual("An old pond!");
+  });
+
+  test('should assign key-value pairs to the haiku object line property', () => {
+    expect(haiku.lines[0]).toEqual("An old pond!");
+  }); 
 });
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,54 @@
 export class Haiku {
   constructor(haikuInput) {
-    // this.haikuInput = haikuInput
+    this.haikuInput = haikuInput;
+    this.lines = {};
   }
+
+  assignLineKeys () {
+    let haikuArray = this.haikuInput.split("\n");
+    this.lines[0] = haikuArray[0];
+    this.lines[1] = haikuArray[1];
+    this.lines[2] = haikuArray[2];
+    return haikuArray
+  }
+
+
 }
-//   lineNumberChecker () {
-//     const lineNumberChecker = (haikuInput)
-//     this.line1 = line1;
-//     this.line2 = line2;
-//     this.line3 = line3;
-//   }
-// }
+
+
 /*
 
+    
+
+
+
+ let haikuInput = haikuInput;
+    const haikuArray = haikuInput.split("\n");
+    haikuInput[0]= haiku.lines[0];
+
+    let haikuInput = haikuInput;
+    const haikuArray = haikuInput.split("\n");
+
+
+// this.currentLineNumber += 1;
+// return this.currentLineNumber;
+
+
+ addLineNumber (haiku) {
+    this.
+  }
+}
+
+  lineGenerator () {
+
+  }
+
+lineNumberChecker () {
+    const lineNumberChecker = (haikuInput)
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
 
 const lineNumberChecker = (haikuInput) => {
 
